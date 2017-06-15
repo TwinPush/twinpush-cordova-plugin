7edd1d23c8d6304fc8fbbcc77e3b293994375999
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2017-06-15 12:59:06 +0200

    iOS plugin initialises TwinPushManager with the AppId, ApiKey and Subdomain parameters

--- src/ios/twinpush.m
+++ src/ios/twinpush.m
@@ -1,8 +1,9 @@
 /********* twinpush.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <TwinPushSDK/TwinPushManager.h>
 
-@interface twinpush : CDVPlugin {
+@interface twinpush : CDVPlugin <TwinPushManagerDelegate> {
   // Member variables go here.
 }
 
@@ -10,6 +11,24 @@
 @end
 
 @implementation twinpush
+
+-(void)pluginInitialize {
+    [super pluginInitialize];
+    NSDictionary* settings = self.commandDelegate.settings;
+
+    NSString* appId = settings[@"TwinPush_AppId".lowercaseString];
+    NSString* apiKey = settings[@"TwinPush_ApiKey".lowercaseString];
+    NSString* subdomain = settings[@"TwinPush_Subdomain".lowercaseString];
+    if (subdomain != nil) {
+        [[TwinPushManager manager] setServerSubdomain:subdomain];
+    }
+    if (appId != nil && apiKey != nil) {
+        [[TwinPushManager manager] setupTwinPushManagerWithAppId:appId apiKey:apiKey delegate:self];
+    }
+    else {
+        NSLog(@"ERROR initializing TwinPush plugin. API Key or APP Id not set in config.xml");
+    }
+}
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
