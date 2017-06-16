487be0cf4b68f16d376ed4fe2cc037ed4b7d20e2
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2017-06-16 15:04:37 +0200

    Added default implementation of required AppDelegate methods to handle token registration and received notification handling

---
+++ src/ios/AppDelegate+TwinPush.h
@@ -0,0 +1,8 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate (TwinPush)
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:( void (^)(UIBackgroundFetchResult))completionHandler;
+
+@end
---
+++ src/ios/AppDelegate+TwinPush.m
@@ -0,0 +1,25 @@
+#import "AppDelegate+TwinPush.h"
+#import <objc/runtime.h>
+#import <TwinPushSDK/TwinPushManager.h>
+
+@implementation AppDelegate (TwinPush)
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[TwinPushManager manager] application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Application did fail registering for remote notifications: %@", error);
+}
+
+- (void) application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"Received notification with contents: %@", userInfo);
+    [[TwinPushManager manager] application:application didReceiveRemoteNotification:userInfo];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    NSLog(@"Received notification with contents: %@", userInfo);
+    [[TwinPushManager manager] application:application didReceiveRemoteNotification:userInfo];
+}
+
+@end
