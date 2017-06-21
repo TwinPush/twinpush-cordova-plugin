e9c3d0ef5cd04bbb810bcacbaa771d12af4292c4
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2017-06-21 12:55:27 +0200

    Created Android version of the plugin, including automatic register, alias and activity events

---
+++ src/android/com/twinpush/cordova/TwinPush.java
@@ -0,0 +1,73 @@
+package com.twinpush.cordova;
+
+import android.util.Log;
+
+import com.twincoders.twinpush.sdk.TwinPushSDK;
+import com.twincoders.twinpush.sdk.entities.TwinPushOptions;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class TwinPush extends CordovaPlugin {
+    private static final String LOG_TAG = "TwinPush";
+
+    private TwinPushSDK twinpush() {
+        return TwinPushSDK.getInstance(cordova.getActivity());
+    }
+
+    @Override
+    protected void pluginInitialize() {
+        super.pluginInitialize();
+
+        TwinPushOptions options = new TwinPushOptions();
+        options.twinPushAppId = preferences.getString("TwinPush_AppId", "");
+        options.twinPushApiKey = preferences.getString("TwinPush_ApiKey", "");
+        options.subdomain = preferences.getString("TwinPush_Subdomain", "app");
+
+        twinpush().setup(options);
+        twinpush().register();
+    }
+
+    @Override
+    public boolean execute(final String action, final JSONArray data, final CallbackContext callbackContext) {
+        if ("setAlias".equals(action)) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    Log.v(LOG_TAG, "setAlias: data=" + data.toString());
+                    try {
+                        String alias = data.getJSONObject(0).getString("alias");
+                        twinpush().register(alias, new TwinPushSDK.OnRegistrationListener() {
+                            @Override
+                            public void onRegistrationSuccess(String currentAlias) {
+                                callbackContext.success("Successfully registered");
+                            }
+
+                            @Override
+                            public void onRegistrationError(Exception exception) {
+                                callbackContext.error(exception.getMessage());
+                            }
+                        });
+                    } catch (JSONException e) {
+                        callbackContext.error(e.getMessage());
+                    }
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        twinpush().activityStart(cordova.getActivity());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        twinpush().activityStop(cordova.getActivity());
+    }
+}
